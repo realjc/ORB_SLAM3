75264d15d03d02a3e251496ee095babac76bdc10
jc <2904152204@qq.com>
2020-12-08 22:43:12 +0800

    can build now

--- src/Optimizer.cc
+++ src/Optimizer.cc
@@ -2971,9 +2971,9 @@ void Optimizer::OptimizeEssentialGraph6DoF(KeyFrame* pCurKF, vector<KeyFrame*> &
             }
 
             string strNameFile = pKFi->mNameFile;
-            cv::Mat imLeft = cv::imread(strNameFile, CV_LOAD_IMAGE_UNCHANGED);
+            cv::Mat imLeft = cv::imread(strNameFile, cv::IMREAD_UNCHANGED);
 
-            cv::cvtColor(imLeft, imLeft, CV_GRAY2BGR);
+            cv::cvtColor(imLeft, imLeft, cv::COLOR_GRAY2BGR);
 
             vector<MapPoint*> vpMapPointsKFi = pKFi->GetMapPointMatches();
             for(int j=0; j<vpMapPointsKFi.size(); ++j)
@@ -3362,8 +3362,8 @@ void Optimizer::OptimizeEssentialGraph(KeyFrame* pCurKF, vector<KeyFrame*> &vpFi
                 cout << "--To much distance correction in EssentGraph: It has connected " << pKFi->GetVectorCovisibleKeyFrames().size() << " KFs" << endl;
             }
             string strNameFile = pKFi->mNameFile;
-            cv::Mat imLeft = cv::imread(strNameFile, CV_LOAD_IMAGE_UNCHANGED);
-            cv::cvtColor(imLeft, imLeft, CV_GRAY2BGR);
+            cv::Mat imLeft = cv::imread(strNameFile, cv::IMREAD_UNCHANGED);
+            cv::cvtColor(imLeft, imLeft, cv::COLOR_GRAY2BGR);
             vector<MapPoint*> vpMapPointsKFi = pKFi->GetMapPointMatches();
             for(int j=0; j<vpMapPointsKFi.size(); ++j)
             {
@@ -6640,9 +6640,9 @@ void Optimizer::LocalBundleAdjustment(KeyFrame* pMainKF,vector<KeyFrame*> vpAdju
             if(bShowImages)
             {
                 string strNameFile = pKFi->mNameFile;
-                cv::Mat imLeft = cv::imread(strNameFile, CV_LOAD_IMAGE_UNCHANGED);
+                cv::Mat imLeft = cv::imread(strNameFile, cv::IMREAD_UNCHANGED);
 
-                cv::cvtColor(imLeft, imLeft, CV_GRAY2BGR);
+                cv::cvtColor(imLeft, imLeft, cv::COLOR_GRAY2BGR);
 
                 int numPointsMono = 0, numPointsStereo = 0;
                 int numPointsMonoBad = 0, numPointsStereoBad = 0;
